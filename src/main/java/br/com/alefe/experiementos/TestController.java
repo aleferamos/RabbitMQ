79f824e03331b2c0c6ad9c03bb49689eeae70d51
Alefe Patrick <aleferamos@lampp-it.com.br>
2023-07-05 09:07:35 -0400

    dgfgffd

--- src/main/java/br/com/alefe/experiementos/TestController.java
+++ src/main/java/br/com/alefe/experiementos/TestController.java
@@ -1,19 +1,12 @@
 package br.com.alefe.experiementos;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import jakarta.validation.constraints.Email;
 import org.springframework.amqp.core.AmqpTemplate;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Lazy;
-import org.springframework.context.annotation.Scope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("test")
-@Scope("singleton")
 public class TestController {
 
     private final AmqpTemplate queueSender;
